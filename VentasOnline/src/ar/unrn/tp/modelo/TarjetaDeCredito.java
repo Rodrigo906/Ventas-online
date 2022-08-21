659a444cde0e6f4990f003fb4ae723a4b72c42b0
Rodrigo906 <82407521+Rodrigo906@users.noreply.github.com>
2022-08-21 16:04:41 -0300

    1 actualizacion

---
+++ VentasOnline/src/ar/unrn/tp/modelo/TarjetaDeCredito.java
@@ -0,0 +1,30 @@
+package ar.unrn.tp.modelo;
+
+public class TarjetaDeCredito {
+	private int numero;
+	private String marca;
+	
+	public TarjetaDeCredito(int numero, String marca) {
+		this.numero = numero;
+		this.marca = marca;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+
+}
